1fefb892134f51411e02d012ca586450823e5602
Borys Chabior <chabiorborys@gmail.com>
2020-06-16 15:14:08 +0200

    7th commit after a break

--- charity/forms.py
+++ charity/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-
+from django.core.exceptions import ValidationError
 from charity.models import Category, Institution
 
 
@@ -7,3 +7,17 @@ class CharityForm(forms.Form):
     category = forms.ModelMultipleChoiceField(queryset=Category.objects.all())
     number_of_items = forms.IntegerField()
     organization = forms.ModelChoiceField(queryset=Institution.objects.all())
+
+class RegistrationForm(forms.Form):
+    first_name = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder':'Imię'}))
+    last_name = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'Nazwisko'}))
+    email = forms.EmailField(label='', widget=forms.EmailInput(attrs={'placeholder': 'Email'}))
+    password = forms.CharField(label='', widget=forms.PasswordInput(attrs={'placeholder':'Hasło'}))
+    re_password = forms.CharField(label='', widget=forms.PasswordInput(attrs={'placeholder':'Powtórz Hasło'}))
+
+    def clean(self):
+        cleaned_data = super().clean()
+        if not self.errors:
+            if cleaned_data['password'] != cleaned_data['re_password']:
+                raise ValidationError("Hasła nie są identyczne")
+        return self.cleaned_data
--- charity/views.py
+++ charity/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 
-from .forms import CharityForm
+from .forms import CharityForm, RegistrationForm
 from .models import Donation, Institution, Category
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login, logout, authenticate
+
+
 class LandingPageView(View):
     def get(self, request):
         sum_of_all_bags = Donation.sum_of_all_bags()
@@ -14,37 +16,59 @@ class LandingPageView(View):
         nonprofits = Institution.all_nonprofits()
         lokals = Institution.all_lokal()
         context = {'sum_of_all_bags': sum_of_all_bags,
-                   'sum_of_all_institutions':sum_of_all_institutions,
-                   'foundations':foundations,
+                   'sum_of_all_institutions': sum_of_all_institutions,
+                   'foundations': foundations,
                    'nonprofits': nonprofits,
-                   'lokals':lokals,
+                   'lokals': lokals,
                    }
         return render(request, 'index.html', context)
 
+
 class AddDonationView(View):
     def get(self, request):
-        form = CharityForm()
-        all_institutions = Institution.objects.all()
-        all_categories = Category.objects.all()
-        context = {'all_institutions':all_institutions,
-                   'all_categories': all_categories,
-                   'form':form
-                   }
-        return render(request, 'form.html', context)
+        if request.user.is_authenticated:
+            form = CharityForm()
+            all_institutions = Institution.objects.all()
+            all_categories = Category.objects.all()
+            context = {'all_institutions': all_institutions,
+                    'all_categories': all_categories,
+                    'form': form
+                    }
+            return render(request, 'form.html', context)
+        else:
+            return redirect('/login/')
+
 
 class LoginView(View):
     def get(self, request):
         return render(request, 'login.html')
 
+    def post(self, request):
+        email = request.POST['email']
+        password = request.POST['password']
+        user = authenticate(request, username=email, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("/")
+        else:
+            return redirect("/register")
+
+
 class RegisterView(View):
     def get(self, request):
+        form = RegistrationForm()
+        return render(request, 'register.html', {'form': form})
+
+    def post(self, request):
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            re_password = password
+            form = User.objects.create_user(first_name=first_name, last_name=last_name, email=email,
+                                            password=password, username=email)
+            form.save()
+            return render(request, 'login.html', {'form': form})
         return render(request, 'register.html')
-
-
-
-
-
-
-
-
-
